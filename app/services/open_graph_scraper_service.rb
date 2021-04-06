404f03b3309de008322e17fc814af40d5b264292
hpneo <hpneo@hotmail.com>
2021-04-06 10:24:19 -0500

    Add fallback for Open Graph scraper

--- app/services/open_graph_scraper_service.rb
+++ app/services/open_graph_scraper_service.rb
@@ -8,7 +8,7 @@ end
 
 OpenGraphReader::Fetcher::HEADERS = {
   Accept: "text/html",
-  "User-Agent": "facebookexternalhit/1.1",
+  "User-Agent": "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
 }.freeze
 
 class OpenGraphScraperService
@@ -16,6 +16,14 @@ class OpenGraphScraperService
     target = OpenGraphReader::Fetcher.new(url)
 
     OpenGraphReader.parse!(target.body, target.url)
+  rescue
+    fallback(url)
+  end
+
+  def self.fallback(url)
+    body = HTTP.use(:auto_inflate).headers(GenericScraperService::HEADERS).follow.get(url).body.to_s
+
+    OpenGraphReader.parse!(body, url)
   rescue
     nil
   end
