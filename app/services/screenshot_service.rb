0c3ac7701c36ea835f5758d105eba4d42dfba98f
hpneo <hpneo@hotmail.com>
2021-04-29 18:21:52 -0500

    Set options only for Heroku

--- app/services/screenshot_service.rb
+++ app/services/screenshot_service.rb
@@ -1,9 +1,17 @@
 class ScreenshotService
   def self.call(url)
-    browser = Ferrum::Browser.new(
-      window_size: [1080, 720],
-      browser_path: ENV['GOOGLE_CHROME_SHIM']
-    )
+    options = {
+      window_size: [1080, 720]
+    }
+
+    if ENV['GOOGLE_CHROME_SHIM'].present?
+      options = options.merge(
+        browser_path: ENV['GOOGLE_CHROME_SHIM'],
+        browser_options: { 'no-sandbox': nil }
+      )
+    end
+
+    browser = Ferrum::Browser.new(options)
     browser.go_to(url)
 
     file = Tempfile.new("screenshot-#{ActiveSupport::Inflector.parameterize(url)}-#{Time.now.to_i}.png")
